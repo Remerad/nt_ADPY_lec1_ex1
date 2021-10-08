3a4c01f4574778705d4070a9b6ff9596d707d9d1
Remerad <jgodgiven@gmail.com>
2021-10-08 14:40:23 +0300

    Готово.

---
+++ dirty_main.py
@@ -0,0 +1,13 @@
+def dirty_funk0(): {
+    print(f'Первая функция dirty_main.py')
+}
+def dirty_funk1(): {
+    print(f'Вторая функция dirty_main.py')
+}
+def dirty_funk2(): {
+    print(f'Третья функция dirty_main.py')
+}
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print("Работает dirty_main.py")
+
---
+++ main.py
@@ -0,0 +1,11 @@
+from application.salary import calculate_salary
+from application.db.people import get_employees
+from dirty_main import *
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    calculate_salary()
+    get_employees()
+    dirty_funk0()
+    dirty_funk1()
+    dirty_funk2()
